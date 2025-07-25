db95791671a16a76781935a6d1b81fcfd7142a5e
DS <aboussoubel@gmail.com>
2025-07-25 21:29:28 +0700

    Page deployement

---
+++ src_todolist_inbox_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkodin_todo"] = self["webpackChunkodin_todo"] || []).push([["src_todolist_inbox_js"],{
+
+/***/ "./src/todolist_inbox.js":
+/*!*******************************!*\
+  !*** ./src/todolist_inbox.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _todolist_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./todolist.js */ \"./src/todolist.js\");\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class extends _todolist_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"] {\n    constructor(name) {\n        super(name);\n        _todolist_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].setInboxTodoList(this);\n    }\n});;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdG9kb2xpc3RfaW5ib3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBcUM7O0FBRXJDLGlFQUFlLGNBQWMsb0RBQVE7QUFDckM7QUFDQTtBQUNBLFFBQVEsb0RBQVE7QUFDaEI7QUFDQSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi10b2RvLy4vc3JjL3RvZG9saXN0X2luYm94LmpzPzcxZTEiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFRvZG9MaXN0IGZyb20gXCIuL3RvZG9saXN0LmpzXCI7XG5cbmV4cG9ydCBkZWZhdWx0IGNsYXNzIGV4dGVuZHMgVG9kb0xpc3Qge1xuICAgIGNvbnN0cnVjdG9yKG5hbWUpIHtcbiAgICAgICAgc3VwZXIobmFtZSk7XG4gICAgICAgIFRvZG9MaXN0LnNldEluYm94VG9kb0xpc3QodGhpcyk7XG4gICAgfVxufTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/todolist_inbox.js\n\n}");
+
+/***/ })
+
+}]);
---
+++ src_todolist_starred_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkodin_todo"] = self["webpackChunkodin_todo"] || []).push([["src_todolist_starred_js"],{
+
+/***/ "./src/todolist_starred.js":
+/*!*********************************!*\
+  !*** ./src/todolist_starred.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _todolist_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./todolist.js */ \"./src/todolist.js\");\n/* harmony import */ var _task_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./task.js */ \"./src/task.js\");\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class extends _todolist_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"] {\n    constructor(name) {\n        super(name);\n        _todolist_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].setStarredTodoList(this);\n    }\n\n    criterion(task) {\n        return task.isStarred();\n    }\n});;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdG9kb2xpc3Rfc3RhcnJlZC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBcUM7QUFDUjs7QUFFN0IsaUVBQWUsY0FBYyxvREFBUTtBQUNyQztBQUNBO0FBQ0EsUUFBUSxvREFBUTtBQUNoQjs7QUFFQTtBQUNBO0FBQ0E7QUFDQSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi10b2RvLy4vc3JjL3RvZG9saXN0X3N0YXJyZWQuanM/Y2EzYSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgVG9kb0xpc3QgZnJvbSBcIi4vdG9kb2xpc3QuanNcIjtcbmltcG9ydCBUYXNrIGZyb20gXCIuL3Rhc2suanNcIjtcblxuZXhwb3J0IGRlZmF1bHQgY2xhc3MgZXh0ZW5kcyBUb2RvTGlzdCB7XG4gICAgY29uc3RydWN0b3IobmFtZSkge1xuICAgICAgICBzdXBlcihuYW1lKTtcbiAgICAgICAgVG9kb0xpc3Quc2V0U3RhcnJlZFRvZG9MaXN0KHRoaXMpO1xuICAgIH1cblxuICAgIGNyaXRlcmlvbih0YXNrKSB7XG4gICAgICAgIHJldHVybiB0YXNrLmlzU3RhcnJlZCgpO1xuICAgIH1cbn07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/todolist_starred.js\n\n}");
+
+/***/ })
+
+}]);
